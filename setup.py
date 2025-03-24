6c8381cd833ea12fb86e1d869b46098d22857bd3
Артём <165937828+engineering-wrench@users.noreply.github.com>
2025-03-24 23:36:40 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+        name="world_api",
+    version="0.1.0",
+    author="EngeenerWerch",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    description='interacte with world',
+    author_email='cyberpony_x@mail.ru',
+    url='https://github.com/engineering-wrench/WorldAPI'  # Укажите ссылку на репозиторий
+)
